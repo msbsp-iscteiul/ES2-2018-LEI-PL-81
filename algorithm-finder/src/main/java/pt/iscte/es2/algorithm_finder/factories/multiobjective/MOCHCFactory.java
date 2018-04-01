4ee0d0bb069d80f6905f852836bf7047f4edb1fd
Mauro Pinto <mauro.s.pinto@gmail.com>
2018-04-01 14:12:03 +0100

    remove MOCHC for lack of configuration

--- algorithm-finder/src/main/java/pt/iscte/es2/algorithm_finder/factories/multiobjective/MOCHCFactory.java
+++ algorithm-finder/src/main/java/pt/iscte/es2/algorithm_finder/factories/multiobjective/MOCHCFactory.java
@@ -2,12 +2,16 @@ package pt.iscte.es2.algorithm_finder.factories.multiobjective;
 
 import org.uma.jmetal.algorithm.multiobjective.mochc.MOCHC;
 import org.uma.jmetal.algorithm.multiobjective.mochc.MOCHCBuilder;
+import org.uma.jmetal.algorithm.multiobjective.nsgaiii.util.EnvironmentalSelection;
+import org.uma.jmetal.operator.impl.selection.BestSolutionSelection;
 import org.uma.jmetal.problem.BinaryProblem;
 import org.uma.jmetal.solution.BinarySolution;
 import org.uma.jmetal.util.AlgorithmBuilder;
+import org.uma.jmetal.util.comparator.DominanceComparator;
 import pt.iscte.es2.algorithm_finder.annotations.BuilderTypes;
 
-@BuilderTypes(solutionType = BinarySolution.class)
+//@BuilderTypes(solutionType = BinarySolution.class)
+//TODO
 public class MOCHCFactory implements AlgorithmBuilder<MOCHC> {
 	private final BinaryProblem problem;
 
@@ -17,6 +21,9 @@ public class MOCHCFactory implements AlgorithmBuilder<MOCHC> {
 
 	@Override
 	public MOCHC build() {
-		return new MOCHCBuilder(problem).build();
+		return new MOCHCBuilder(problem)
+			.setParentSelection(new BestSolutionSelection<>(new DominanceComparator<>()))
+			.setNewGenerationSelection(new BestSolutionSelection(new DominanceComparator()))
+			.build();
 	}
 }
