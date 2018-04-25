03ca413eb483d0cd37a848881188949652353471
Michael Carvalho <mrmco@iscte-iul.pt>
2018-04-25 18:09:22 +0100

    Line format

--- backend/src/main/java/pt/iscte/es2/ApplicationConstants.java
+++ backend/src/main/java/pt/iscte/es2/ApplicationConstants.java
@@ -4,19 +4,27 @@ package pt.iscte.es2;
  * Application Constants to be used in the Application
  */
 public class ApplicationConstants {
-    /** Root package for the application. Used for Spring annotation configuration, etc. */
-    public static final String APPLICATION_ROOT_PACKAGE = "pt.iscte.es2";
+	/**
+	 * Root package for the application. Used for Spring annotation configuration, etc.
+	 */
+	public static final String APPLICATION_ROOT_PACKAGE = "pt.iscte.es2";
 
-    /** Location of BeanConfig class */
-    public static final String ANNOTATION_CONFIG_PACKAGE = "pt.iscte.es2";
+	/**
+	 * Location of BeanConfig class
+	 */
+	public static final String ANNOTATION_CONFIG_PACKAGE = "pt.iscte.es2";
 
-    /** Location of Spring-Data JPA repositories */
-    public static final String DAO_PACKAGE = "pt.iscte.es2.dao";
+	/**
+	 * Location of Spring-Data JPA repositories
+	 */
+	public static final String DAO_PACKAGE = "pt.iscte.es2.dao";
 
-    /** Root path for the service endpoints */
-    public static final String APPLICATION_PATH = "api";
-    public static final String UPLOAD_PATH = APPLICATION_PATH + "/upload";
+	/**
+	 * Root path for the service endpoints
+	 */
+	public static final String APPLICATION_PATH = "api";
+	public static final String UPLOAD_PATH = APPLICATION_PATH + "/upload";
 	public static final String HISTORY_PATH = APPLICATION_PATH + "/history";
 	public static final String EXECUTION_PATH = "backend/target/";
-    public static final String JARS_PATH = EXECUTION_PATH + "jars/";
+	public static final String JARS_PATH = EXECUTION_PATH + "jars/";
 }
