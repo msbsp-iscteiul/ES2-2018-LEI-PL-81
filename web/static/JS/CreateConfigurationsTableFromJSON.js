50674e9f35d8078e8b181ec455343f13a383561c
Maria João Canedo <mjtco@iscte-iul.pt>
2018-05-20 19:16:32 +0100

    execution request and review

--- web/static/JS/CreateConfigurationsTableFromJSON.js
+++ web/static/JS/CreateConfigurationsTableFromJSON.js
@@ -1,5 +1,5 @@
 $(function () {
-    $.get('http://172.17.5.62:8080/api/optimization/searchoptimizationconfigurationbyemail', {email: window.SESSION_EMAIL}, function (data) {
+    $.get('http://172.17.10.175:8080/api/optimization/searchoptimizationconfigurationbyemail', {email: window.SESSION_EMAIL}, function (data) {
         var tableContent = '';
         $.each(data.result.summary, function (i) {
 
@@ -7,8 +7,8 @@ $(function () {
             tableContent += '<th scope="row">' + data.result.summary[i].problemName + '</th>';
             tableContent += '<td>' + data.result.summary[i].description + '</td>';
             tableContent += '<td>' + new Date(data.result.summary[i].createdDate).toLocaleDateString() + '</td>';
-            tableContent += '<td><a href="/requestdetails/' + data.result.summary[i].id +'" class="btn btn-primary">Open</a></td>';
-            tableContent += '<td><button class="btn btn-primary">Execute</button></td>';
+            tableContent += '<td><a href="/requestdetails/' + data.result.summary[i].id + '" class="btn btn-primary">Open</a></td>';
+            tableContent += '<td><a href="/processing/' + data.result.summary[i].id + '" class="btn btn-primary">Execute</a></td>';
             tableContent += '</tr>';
         });
         $('#tableDiv').html(tableContent);
